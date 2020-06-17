6ac6f9d5c9528b14b107c4d63968269c7df5bf86
brayan-mesquita <brayanmesquita@gmail.com>
2020-06-17 10:31:57 -0500

    modificações

--- Siafem/atalhos.py
+++ Siafem/atalhos.py
@@ -1,57 +1,51 @@
-import pyautogui
-senha = 'xxx'
+import pyautogui, time
 #senha
-while senha != 'senha':
-    senha = pyautogui.password(text='Senha de acesso:', title='Usuario Brayan', default='', mask='*')
-    if senha == '0':
-        break
-#escolha ano
-ano = pyautogui.prompt(text="Exercício: 18, 19, 20-Enter..." , title='Ano para conculta' , default='')
-if ano == '18':
-    ano = '2018'
-elif ano == '19':
-    ano = '2019'
-else:
-    ano = '2020'
+senha = pyautogui.password(text='Senha de acesso:', title='Usuario Brayan', default='', mask='*')
 #login
-pyautogui.PAUSE = 1
-pyautogui.click(x=447, y=271)
-pyautogui.PAUSE = 1
-pyautogui.write(ano, interval=0.01)
+def login(senha, ano):
+    pyautogui.PAUSE = 1
+    pyautogui.click(x=447, y=271)
+    pyautogui.PAUSE = 1.5
+    pyautogui.write('telnet 172.22.0.85 23', interval=0.07)
+    pyautogui.PAUSE = 1
+    pyautogui.hotkey('enter')
+    time.sleep(2)
+    pyautogui.write('sesdec', interval=0.02)
+    pyautogui.hotkey('enter')
+    pyautogui.PAUSE = 1.5
+    pyautogui.write('sesdec', interval=0.02)
+    pyautogui.hotkey('enter')
+    time.sleep(2)
+    pyautogui.write(ano, interval=0.02)
+    pyautogui.hotkey('enter')
+    time.sleep(2)
+    pyautogui.write('01606363298', interval=0.02)
+    time.sleep(2)
+    pyautogui.write(senha, interval=0.02)
+    pyautogui.hotkey('enter')
+    time.sleep(2)
+    pyautogui.hotkey('enter')
 
-#janela teimosa
+def detaconta(conta):
+    time.sleep(2)
+    pyautogui.write(conta, interval=0.02)
+
+
+#janela 
 while True:
-    opcao = pyautogui.prompt(text="Fechar='0'\nLogin='1'\n---------------\nLançar NE='lne'\nCons NE='cne'\n---------------\nDetalhaConta='dc' " , title='SIAFEM' , default='')
+    opcao = pyautogui.prompt(text="Fechar='0'\nLogin='Ano 19, 20...'\n---------------\nDetalhaConta='dc' " , title='SIAFEM' , default='')
     if opcao == '0':
         break
-    elif opcao == '1': #login de novo
-        pyautogui.PAUSE = 1
-        pyautogui.click(x=447, y=271)
-        pyautogui.PAUSE = 1
-        pyautogui.write('2020', interval=0.02)
-    elif opcao == 'lne':
-        pyautogui.PAUSE = 1
-        pyautogui.click(x=447, y=271)
-        pyautogui.PAUSE = 1
-        pyautogui.write('sesdec', interval=0.02)
-        pyautogui.hotkey('enter')
-        pyautogui.write('sesdec', interval=0.02)
-        pyautogui.hotkey('enter')
-        pyautogui.write('15003', interval=0.02)
-        pyautogui.hotkey('enter')
-        pyautogui.write('0000001', interval=0.02)
-        pyautogui.hotkey('tab')
-        pyautogui.write('sesdec', interval=0.02)
-        pyautogui.hotkey('tab')
-        pyautogui.write('sesdec', interval=0.02)
-        pyautogui.hotkey('enter')
-        pyautogui.PAUSE = 1
+    elif opcao == '20':
+        login(senha, '2020')
+    elif opcao == '19':
+        login(senha, '2019')
     elif opcao == 'dc':
-        conta = pyautogui.prompt(text="Ex.: xxxxxx-DEZ" , title='Conta e Mês separado por -', default='')
-        pyautogui.PAUSE = 1.2
-        pyautogui.click(x=447, y=271)
-        pyautogui.PAUSE = 1
-        pyautogui.write(conta, interval=0.01)
+        conta = pyautogui.prompt(text='Conta:', title='Informe a conta')
+        detaconta(str(conta))
+        time.sleep(1.5)
+        pyautogui.hotkey('enter')
+
 
 
    
